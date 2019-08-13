04529632281be348a1921cce75a4f163477cab93
Keheliya Gallaba <keheliya.gallaba@gmail.com>
2019-08-13 15:17:29 -0400

    Create yourbased.sh

---
+++ yourbased.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+set -ex
+export DEBIAN_FRONTEND=noninteractive
+export CI=true
+export TRAVIS=true
+export CONTINUOUS_INTEGRATION=true
+export USER=travis
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+export RAILS_ENV=test
+export RACK_ENV=test
+export MERB_ENV=test
+export JRUBY_OPTS="--server -Dcext.enabled=false -Xcompile.invokedynamic=false"
+apt-get update && apt-get install -y tzdata
+gem install bundler -v 2.0.1
+# before_install
+gem install bundler -v '< 2'
+# install
+bundle install --jobs=3 --retry=3
+# before_script
+#mysql -e 'CREATE DATABASE dummy_test;' -uroot
+#psql -c 'CREATE DATABASE dummy_test;' -U postgres
+RAILS_ENV=test bundle exec rake db:drop
+RAILS_ENV=test bundle exec rake db:create
+curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64 > ./cc-test-reporter
+chmod +x ./cc-test-reporter
+./cc-test-reporter before-build
+cat Gemfile.lock
+# script
+bundle exec rubocop
+RAILS_ENV=test bundle exec rake --trace db:setup db:migrate spec
