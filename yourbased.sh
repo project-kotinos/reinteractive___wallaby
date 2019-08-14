5826db6839e33399c5899cafda5ea55d3e617047
Keheliya Gallaba <keheliya.gallaba@gmail.com>
2019-08-14 17:37:47 -0400

    remove redundant bundler

--- yourbased.sh
+++ yourbased.sh
@@ -12,7 +12,7 @@ export RACK_ENV=test
 export MERB_ENV=test
 export JRUBY_OPTS="--server -Dcext.enabled=false -Xcompile.invokedynamic=false"
 apt-get update && apt-get install -y tzdata
-gem install bundler -v 2.0.1
+# gem install bundler -v 2.0.1
 # before_install
 gem install bundler -v '< 2'
 # install
