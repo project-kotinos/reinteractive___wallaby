2ff36fcecf1dd8bf981ec917d190eb63cfbb1060
Keheliya Gallaba <keheliya.gallaba@gmail.com>
2019-08-15 14:19:33 -0400

    Update yourbased.sh

--- yourbased.sh
+++ yourbased.sh
@@ -11,7 +11,7 @@ export RAILS_ENV=test
 export RACK_ENV=test
 export MERB_ENV=test
 export JRUBY_OPTS="--server -Dcext.enabled=false -Xcompile.invokedynamic=false"
-apt-get update && apt-get install -y tzdata
+apt-get update && apt-get install -y tzdata mysql-client-5.7 mysql-server-5.7 postgresql
 # gem install bundler -v 2.0.1
 # before_install
 gem install bundler -v '< 2'
